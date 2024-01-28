ad52ef362a1841512415b18f6d652f5835a72f7f
Maksym Rybalkin <maksym.rybalkin@intellias.com>
2024-01-28 23:12:15 +0100

    remove go work

--- power/power.go
+++ power/power.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// Pow returns base^exponent.
 func Pow[T constraints.Float](base, exponent T) float64 {
 	return math.Pow(float64(base), float64(exponent))
 }
